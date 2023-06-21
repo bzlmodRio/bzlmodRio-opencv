5fa571f88084bed7a2769c0e2f10eeb25777a6ab
PJ Reiniger <pj.reiniger@gmail.com>
2023-06-21 15:59:51 -0400

    Better mac gen (#3)
    * Update gen
    
    * Lint
    
    * Regen
    
    * Update gen

--- generate/get_opencv_dependencies.py
+++ generate/get_opencv_dependencies.py
@@ -65,6 +65,7 @@ def get_opencv_dependencies():
         sources="sources",
         resources=_default_all_platforms(),
         has_jni=True,
+        has_install_name_patches=False,
     )
 
     group.create_java_dependency(
