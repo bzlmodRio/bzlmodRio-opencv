400f0530bf3d2f4d24e32f33f6d4fc0a41df2ff6
PJ Reiniger <pj.reiniger@gmail.com>
2023-10-15 11:45:11 -0400

    Updating gen

--- generate/generate.py
+++ generate/generate.py
@@ -26,7 +26,7 @@ def main():
 
     clean_existing_version(REPO_DIR)
     generate_module_project_files(
-        REPO_DIR, group, mandatory_dependencies, include_windows_arm_compiler=True
+        REPO_DIR, group, mandatory_dependencies
     )
     generate_group(output_dir, group, force_tests=args.force_tests)
 
