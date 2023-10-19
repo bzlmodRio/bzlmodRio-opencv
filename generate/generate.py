06b5678d601a868d8dc02a63c41e8695eb66c6d7
PJ Reiniger <pj.reiniger@gmail.com>
2023-10-19 01:27:02 +0100

    Update toolchains version (#9)

--- generate/generate.py
+++ generate/generate.py
@@ -25,9 +25,7 @@ def main():
     mandatory_dependencies = create_default_mandatory_settings(GenericCliArgs(args))
 
     clean_existing_version(REPO_DIR)
-    generate_module_project_files(
-        REPO_DIR, group, mandatory_dependencies, include_windows_arm_compiler=True
-    )
+    generate_module_project_files(REPO_DIR, group, mandatory_dependencies)
     generate_group(output_dir, group, force_tests=args.force_tests)
 
 
